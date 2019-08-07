fd1bb119dd5b926b875a9ca7ff8907b6c377fe9e
harika91 <harika91@users.noreply.github.com>
2019-08-07 14:20:07 -0500

    Update PolyphenFasta.java

--- src/main/java/edu/mcw/rgd/ratcn/PolyphenFasta.java
+++ src/main/java/edu/mcw/rgd/ratcn/PolyphenFasta.java
@@ -71,6 +71,9 @@ public class PolyphenFasta extends VariantProcessingBase {
     }
     public void run(int sampleId) throws Exception {
 
+        VariantDAO vdao = new VariantDAO();
+        varTable = vdao.getVariantTable(sampleId);
+
         List<String> chromosomes = getChromosomes(sampleId);
 
         for( String chr: chromosomes ) {
@@ -80,9 +83,7 @@ public class PolyphenFasta extends VariantProcessingBase {
 
     public void run(int sampleId, String chr) throws Exception {
 
-        VariantDAO vdao = new VariantDAO();
-        varTable = vdao.getVariantTable(sampleId);
-       
+
         // read all log lines from polyphen
         // extract protein acc ids for lines starting with "ERROR: Unable to locate protein entry ";
         Set<String> setOfProteinAccIds = readProteinAccIds(sampleId, chr);
