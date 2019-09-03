706bbb9fec02a28993eb5eca9e835c413abddac3
harika91 <harika91@users.noreply.github.com>
2019-09-03 12:49:07 -0500

    Update polyphen scripts

--- src/main/dist/run_polyphen_chr.sh
+++ src/main/dist/run_polyphen_chr.sh
@@ -2,67 +2,52 @@
 
 . /etc/profile
 APPHOME=/home/rgddata/pipelines/ratStrainLoader
+POLYDIR=$APPHOME/polyphen
 LOGDIR=$APPHOME/logs
 LOG4J_INFO=-Dlog4j.configuration=file:$APPHOME/properties/log4j.properties
 # set -Djava.security.egd to avoid JDBC connection reset when using many processes: https://community.oracle.com/thread/943911
-DBCONN_INFO="-Dspring.config=../properties/reed.xml -Djava.security.egd=file:/dev/../dev/urandom"
+DBCONN_INFO="-Dspring.config=../properties/marcus.xml -Djava.security.egd=file:/dev/../dev/urandom"
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
-EMAIL_LIST=mtutaj@mcw.edu
-$OUTDIR=/data/rat/output
-$RESDIR=/data/rat/results
+EMAIL_LIST=hsnalabolu@mcw.edu,mtutaj@mcw.edu
 
 
-# generate polyphen input file
-echo ""
-echo "generating polyphen input file for sample $1 and chr$2"
+
+echo "  running polyphen, custom fasta, for sample $1 and chr$2"
 cd $APPHOME
-java $DBCONN_INFO $LOG4J_INFO -jar ratStrainLoader.jar \
-  --tool Polyphen \
+java $DBCONN_INFO $LOG4J_INFO -jar lib/ratStrainLoader.jar \
+  --tool Polyphen --fasta \
   --sample $1 --chr $2 \
-  --outDir $OUTDIR \
-  2>&1 > $LOGDIR/poly$1.$2.log
-
-
-# run polyphen
-echo "  running polyphen for sample $1 and chr$2"
-./run_pph.sh $1 $2
-
-#load polyphen results into database
-cd $APPHOME
-java $DBCONN_INFO $LOG4J_INFO -jar ratStrainLoader.jar \
-  --tool PolyphenLoader \
-  --sample $1 --chr $2 \
-  --outputDir $OUTDIR \
-  --resultsDir $RESDIR \
-  > $LOGDIR/polyload$1.$2.log
-echo `cat $LOGDIR/polyload$1.$2.log`
-
-# rerun polyphen for proteins that are not in polyphen database
-# by supplying a fasta file with those proteins to the polyphen
-
-echo "  re-running polyphen, custom fasta, for sample $1 and chr$2"
-cd $APPHOME
-java $DBCONN_INFO $LOG4J_INFO -jar ratStrainLoader.jar \
-  --tool PolyphenFasta \
-  --sample $1 --chr $2 \
-  --outputDir $OUTDIR \
-  --resultsDir $RESDIR \
+  --outDir $POLYDIR \
+  --resultsDir $POLYDIR \
   > $LOGDIR/polyfa$1.$2.log
 
-if [ -s $OUTDIR/Sample$1.$2.PolyPhenInput.fasta ]
+
+echo "  starting polyphen ..."
+
+
+if [ -s $POLYDIR/Sample$1.$2.PolyPhenInput.fasta ]
 then
   ./run_pph.sh $1 $2 fasta
+else
+  echo "*** NO DATA -- ABORTING SAMPLE $1 chr $2"
+  rm $POLYDIR/ErrorFile_Sample$1.$2.PolyPhen.error
+  rm $POLYDIR/Sample$1.$2.PolyPhenInput
+  rm $POLYDIR/Sample$1.$2.PolyPhenInput.info
+  rm $POLYDIR/$1.$2.log
+  rm $LOGDIR/polyfa$1.$2.log
+  exit 5
 fi
+
 
 
 #load polyphen results into database
 echo "  loading results $1.$2 into db..."
 cd $APPHOME
-java $DBCONN_INFO $LOG4J_INFO -jar ratStrainLoader.jar \
+java $DBCONN_INFO $LOG4J_INFO -jar lib/ratStrainLoader.jar \
   --tool PolyphenLoader \
   --sample $1 --chr $2 \
-  --outputDir $OUTDIR \
-  --resultsDir $RESDIR \
+  --outputDir $POLYDIR \
+  --resultsDir $POLYDIR \
   > $LOGDIR/polyload$1.$2.log
 echo `cat $LOGDIR/polyload$1.$2.log`
 
