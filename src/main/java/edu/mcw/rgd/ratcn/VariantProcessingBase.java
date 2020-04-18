d79e9b41e8e252fcf2d0aa9372d3cc94ee204f02
harika91 <harika91@users.noreply.github.com>
2020-04-17 19:12:18 -0500

    Changes to load variants to new structure

--- src/main/java/edu/mcw/rgd/ratcn/VariantProcessingBase.java
+++ src/main/java/edu/mcw/rgd/ratcn/VariantProcessingBase.java
@@ -3,6 +3,7 @@ package edu.mcw.rgd.ratcn;
 import edu.mcw.rgd.dao.DataSourceFactory;
 import edu.mcw.rgd.dao.spring.StringListQuery;
 import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.object.BatchSqlUpdate;
 import org.springframework.jdbc.object.SqlUpdate;
 
 import javax.sql.DataSource;
@@ -184,6 +185,65 @@ public class VariantProcessingBase {
                 results.add(file);
             }
         }
+    }
+    public List<VariantMapData> getVariants(int speciesKey, int mapKey,String chr) throws Exception{
+        String sql = "SELECT * FROM variant v inner join variant_map_data vm on v.rgd_id = vm. rgd_id WHERE v.species_type_key = ? and vm.map_key = ? and vm.chromosome=?";
+        VariantMapQuery q = new VariantMapQuery(getVariantDataSource(), sql);
+        q.declareParameter(new SqlParameter(Types.INTEGER));
+        q.declareParameter(new SqlParameter(Types.INTEGER));
+        q.declareParameter(new SqlParameter(Types.VARCHAR));
+        return q.execute(speciesKey,mapKey,chr);
+    }
+    public void insertVariants(List<VariantMapData> mapsData)  throws Exception{
+        BatchSqlUpdate sql1 = new BatchSqlUpdate(this.getVariantDataSource(),
+                "INSERT INTO variant (\n" +
+                        " RGD_ID,REF_NUC, VARIANT_TYPE, VAR_NUC, RS_ID, CLINVAR_ID, SPECIES_TYPE_KEY)\n" +
+                        "VALUES (\n" +
+                        "  ?,?,?,?,?,?,?)",
+                new int[]{Types.INTEGER,Types.VARCHAR,Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.VARCHAR,Types.INTEGER}, 10000);
+        sql1.compile();
+        for( VariantMapData v: mapsData) {
+            long id = v.getId();
+            sql1.update(id, v.getReferenceNucleotide(), v.getVariantType(), v.getVariantNucleotide(), v.getRsId(), v.getClinvarId(), v.getSpeciesTypeKey());
+
+        }
+        sql1.flush();
+    }
+    public void insertVariantMapData(List<VariantMapData> mapsData)  throws Exception{
+        BatchSqlUpdate sql2 = new BatchSqlUpdate(this.getVariantDataSource(),
+                "INSERT INTO variant_map_data (\n" +
+                        " RGD_ID,CHROMOSOME,START_POS,END_POS,PADDING_BASE,GENIC_STATUS,MAP_KEY)\n" +
+                        "VALUES (\n" +
+                        " ?,?,?,?,?,?,?)",
+                new int[]{Types.INTEGER,Types.VARCHAR, Types.INTEGER, Types.INTEGER, Types.VARCHAR,Types.VARCHAR, Types.INTEGER}, 10000);
+        sql2.compile();
+        for( VariantMapData v: mapsData) {
+            long id = v.getId();
+            sql2.update(id, v.getChromosome(), v.getStartPos(), v.getEndPos(), v.getPaddingBase(), v.getGenicStatus(), v.getMapKey());
+        }
+        sql2.flush();
+    }
+    public int insertVariantSample(List<VariantSampleDetail> sampleData) throws Exception {
+        BatchSqlUpdate bsu= new BatchSqlUpdate(this.getVariantDataSource(),
+                "INSERT INTO variant_sample_detail (\n" +
+                        " RGD_ID,SOURCE,SAMPLE_ID,TOTAL_DEPTH,VAR_FREQ,ZYGOSITY_STATUS,ZYGOSITY_PERCENT_READ," +
+                        "ZYGOSITY_POSS_ERROR,ZYGOSITY_REF_ALLELE,ZYGOSITY_NUM_ALLELE,ZYGOSITY_IN_PSEUDO,QUALITY_SCORE)\n" +
+                        "VALUES (?,?,?,?,?,?,?," +
+                        "?,?,?,?,?)",
+                new int[]{Types.INTEGER,Types.VARCHAR,Types.INTEGER, Types.INTEGER, Types.INTEGER,Types.VARCHAR, Types.INTEGER,
+                        Types.VARCHAR,Types.VARCHAR, Types.INTEGER,Types.VARCHAR, Types.INTEGER}, 10000);
+        bsu.compile();
+        for(VariantSampleDetail v: sampleData ) {
+            bsu.update(v.getId(), v.getSource(), v.getSampleId(),v.getDepth(),v.getVariantFrequency(),v.getZygosityStatus(),v.getZygosityPercentRead(),
+                    v.getZygosityPossibleError(),v.getZygosityRefAllele(),v.getZygosityNumberAllele(),v.getZygosityInPseudo(),v.getQualityScore());
+        }
+        bsu.flush();
+        // compute nr of rows affected
+        int totalRowsAffected = 0;
+        for( int rowsAffected: bsu.getRowsAffected() ) {
+            totalRowsAffected += rowsAffected;
+        }
+        return totalRowsAffected;
     }
     public DataSource getVariantDataSource() throws Exception{
         return DataSourceFactory.getInstance().getDataSource("Variant");
