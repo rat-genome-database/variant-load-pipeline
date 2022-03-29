e52b0e4fd27125494bafe1078145ca59faeaa713
llamersmcw <llamers@mcw.edu>
2022-03-29 12:57:05 -0500

    running post processing for rat, dog, vervet

--- src/main/dist/postProcessingEva.sh
+++ src/main/dist/postProcessingEva.sh
@@ -35,32 +35,32 @@ cd $APPHOME
 
   java $JAVA_INFO \
     --tool VariantPostProcessing \
-    --mapKey 1311 --fastaDir "/ref/fasta/chlSab2" \
+    --mapKey 1311 --fastaDir "/ref/fasta/veroWho" \
     --verifyIfInRgd \
     > "$WORKDIR/vpp1311.log"
   wait
   echo "  EVA species Green Monkey OK"
 
-  java $JAVA_INFO \
-    --tool VariantPostProcessing \
-    --mapKey 910 --fastaDir "/ref/fasta/susScr3" \
-    --verifyIfInRgd \
-    > "$WORKDIR/vpp910.log"
-  wait
-  echo "  EVA species Pig 10.2 OK"
-
-  java $JAVA_INFO \
-    --tool VariantPostProcessing \
-    --mapKey 911 --fastaDir "/ref/fasta/susScr11" \
-    --verifyIfInRgd \
-    > "$WORKDIR/vpp911.log"
-  wait
-  echo "  EVA species Pig 11.1 OK"
-
-  java $JAVA_INFO \
-    --tool VariantPostProcessing \
-    --mapKey 35 --fastaDir "/ref/fasta/mm38" \
-    --verifyIfInRgd \
-    > "$WORKDIR/vpp35.log"
-  wait
-  echo "  EVA species Mouse OK"
+#  java $JAVA_INFO \
+#    --tool VariantPostProcessing \
+#    --mapKey 910 --fastaDir "/ref/fasta/susScr3" \
+#    --verifyIfInRgd \
+#    > "$WORKDIR/vpp910.log"
+#  wait
+#  echo "  EVA species Pig 10.2 OK"
+#
+#  java $JAVA_INFO \
+#    --tool VariantPostProcessing \
+#    --mapKey 911 --fastaDir "/ref/fasta/susScr11" \
+#    --verifyIfInRgd \
+#    > "$WORKDIR/vpp911.log"
+#  wait
+#  echo "  EVA species Pig 11.1 OK"
+#
+#  java $JAVA_INFO \
+#    --tool VariantPostProcessing \
+#    --mapKey 35 --fastaDir "/ref/fasta/mm38" \
+#    --verifyIfInRgd \
+#    > "$WORKDIR/vpp35.log"
+#  wait
+#  echo "  EVA species Mouse OK"
