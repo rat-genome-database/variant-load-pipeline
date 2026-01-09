1a2eda3f35db638aa437ee233d5bd2f8bd26101a
llamersmcw <llamers@mcw.edu>
2026-01-09 13:55:27 -0600

    postprocessing: check if RNA is divisible by 3 is only for rn3.4

--- src/main/java/edu/mcw/rgd/ratcn/VariantPostProcessing.java
+++ src/main/java/edu/mcw/rgd/ratcn/VariantPostProcessing.java
@@ -517,38 +517,41 @@ public class VariantPostProcessing extends VariantProcessingBase {
                 }
             }
 
-            // Check for rna evenly divisable by 3 or log as error
             String transcriptErrorFound = "F";
-            if (refDna.length() % 3 != 0) {
-                writeError(variantId+":"+transcriptRgdId+":"+refDna.length()+":"+((new Date()).toString())+":TRIPLETERROR\n", mapKey);
-                if( isDbgLogging() ) {
-                    getLogWriter().write("************************* Warning in transcript rna length : see error_rga.txt file  ************************\n");
+            if (mapKey==60) {
+                // Check for rna evenly divisable by 3 or log as error
+                
+                if (refDna.length() % 3 != 0) {
+                    writeError(variantId + ":" + transcriptRgdId + ":" + refDna.length() + ":" + ((new Date()).toString()) + ":TRIPLETERROR\n", mapKey);
+                    if (isDbgLogging()) {
+                        getLogWriter().write("************************* Warning in transcript rna length : see error_rga.txt file  ************************\n");
+                    }
+                    // Use this later to update VARIANT+TRANSCRIPT table with error
+                    transcriptErrorFound = "T";
                 }
-                // Use this later to update VARIANT+TRANSCRIPT table with error
-                transcriptErrorFound = "T";
-            }
-            // make it divisible by 3
-            if (refDna.length() % 3 != 0) {
-                refDna.replace(0, refDna.length(), refDna.substring(0, refDna.length() - (refDna.length() % 3)));
-                if( isDbgLogging() ) {
-                    getLogWriter().write(" RefDna fixed div 3 length =  : " + refDna.length() + " mod " + (refDna.length() % 3) + "\n");
+                // make it divisible by 3
+                if (refDna.length() % 3 != 0) {
+                    refDna.replace(0, refDna.length(), refDna.substring(0, refDna.length() - (refDna.length() % 3)));
+                    if (isDbgLogging()) {
+                        getLogWriter().write(" RefDna fixed div 3 length =  : " + refDna.length() + " mod " + (refDna.length() % 3) + "\n");
+                    }
                 }
-            }
-            if (varDna.length() % 3 != 0) {
-                varDna.replace(0, varDna.length(), varDna.substring(0, varDna.length() - (varDna.length() % 3)));
-                if( isDbgLogging() ) {
-                    getLogWriter().write(" VarDna fixed div 3 length =  : " + varDna.length() + " mod " + (varDna.length() % 3) + "\n");
+                if (varDna.length() % 3 != 0) {
+                    varDna.replace(0, varDna.length(), varDna.substring(0, varDna.length() - (varDna.length() % 3)));
+                    if (isDbgLogging()) {
+                        getLogWriter().write(" VarDna fixed div 3 length =  : " + varDna.length() + " mod " + (varDna.length() % 3) + "\n");
+                    }
                 }
-            }
 
-            // Now test to see if the variant was in an area eliminated by the divisable by 3 truncation process
-            if ( variantRelPos < 1 ) {
-                writeError(variantId+":"+transcriptRgdId+":"+refDna.length()+":"+new Date().toString()+":SKIPPED\n", mapKey);
-                if( isDbgLogging() ) {
-                    getLogWriter().write("************************* Error in transcript variant in trimmed area : skipping see error_rga.txt file  ************************\n");
-                }
-                return false; // return false to insert new row into VARIANT_TRANSCRIPT: at least variant location will be available
+                // Now test to see if the variant was in an area eliminated by the divisable by 3 truncation process
+                if (variantRelPos < 1) {
+                    writeError(variantId + ":" + transcriptRgdId + ":" + refDna.length() + ":" + new Date().toString() + ":SKIPPED\n", mapKey);
+                    if (isDbgLogging()) {
+                        getLogWriter().write("************************* Error in transcript variant in trimmed area : skipping see error_rga.txt file  ************************\n");
+                    }
+                    return false; // return false to insert new row into VARIANT_TRANSCRIPT: at least variant location will be available
 
+                }
             }
 
             if( isDbgLogging() ) {
